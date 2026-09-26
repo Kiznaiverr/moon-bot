40160f2273d674c30b4777ac6d1f304a354a79c1
Kiznavierr <atqiyaakmal24@gmail.com>
2026-09-26 14:47:44 +0700

    lupa jir:v

--- kirara.js
+++
@@ -1,8 +0,0 @@
-import { Kirara } from "@kiznavierr/kirara";
-
-const kirara = new Kirara("genshin"); // or "hsr" or "zzz"
-
-// Get player summary
-kirara
-  .getPlayerSummary("618285856", { lang: "en" })
-  .then((data) => console.log(data));
